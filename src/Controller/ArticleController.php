c1240d7b06cbf9b5aed68247ce10122abbb418d9
Lucky31001 <kilian.dumaire@gmail.com>
2024-10-02 11:35:49 +0200

    WIP

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -16,6 +16,7 @@ use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class ArticleController extends AbstractController
 {

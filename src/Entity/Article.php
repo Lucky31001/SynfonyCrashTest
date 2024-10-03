8e3c20539f7eb729c0dba786e3769467287d3d2c
Lucky31001 <kilian.dumaire@gmail.com>
2024-10-04 01:24:53 +0200

    prettier le retour

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -15,133 +15,134 @@ class Article
     #[ORM\Id]
     #[ORM\Column(unique: true)]
     #[ORM\GeneratedValue]
-private ?int $id;
+    private ?int $id;
 
     #[ORM\Column(type: 'string', length: 255)]
-private string $title;
+    private string $title;
 
     #[ORM\Column(type: 'string', length: 1024, nullable: true)]
-private string $content;
+    private string $content;
 
     #[ORM\Column(type: 'string', length: 1024, nullable: true)]
-private string $image;
+    private string $image;
 
     #[ORM\Column(type: 'integer', options: ['default' => 0], nullable: true)]
-private int $fav;
+    private int $fav;
 
     #[ORM\Column(type: 'integer')]
-private int $price;
+    private int $price;
 
     #[ORM\Column(type: 'integer', options: ['default' => 20])]
-private int $tva;
+    private int $tva;
 
     #[ORM\ManyToOne(targetEntity: Category::class, cascade: ['persist'])]
     #[ORM\JoinColumn(nullable: true)]
-private ?Category $category;
+    private ?Category $category;
 
     #[ORM\Column]
-private ?bool $show = null;
+    private ?bool $show = null;
     /**
      * @var Collection<int, Favorite>
      */
     #[ORM\OneToMany(targetEntity: Favorite::class, mappedBy: 'aritcle_id')]
-private Collection $favorites;
+    private Collection $favorites;
 
-public function __construct()
-{
-    $this->setFav(0);
-    $this->setShow(1);
-    $this->favorites = new ArrayCollection();
-}
+    public function __construct()
+    {
+        $this->setFav(0);
+        $this->setShow(1);
+        $this->favorites = new ArrayCollection();
+    }
 
-public function getId(): ?int
-{
-    return $this->id;
-}
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
 
-public function setId(?int $id): void
-{
-    $this->id = $id;
-}
+    public function setId(?int $id): void
+    {
+        $this->id = $id;
+    }
 
-public function getTitle(): string
-{
-    return $this->title;
-}
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
 
-public function setTitle(string $title): void
-{
-    $this->title = $title;
-}
+    public function setTitle(string $title): void
+    {
+        $this->title = $title;
+    }
 
-public function getContent(): string
-{
-    return $this->content;
-}
+    public function getContent(): string
+    {
+        return $this->content;
+    }
 
-public function setContent(string $content): void
-{
-    $this->content = $content;
-}
+    public function setContent(string $content): void
+    {
+        $this->content = $content;
+    }
 
-public function getImage(): string
-{
-    return $this->image;
-}
+    public function getImage(): string
+    {
+        return $this->image;
+    }
 
-public function setImage(string $image): void
-{
-    $this->image = $image;
-}
+    public function setImage(string $image): void
+    {
+        $this->image = $image;
+    }
 
-public function getFav(): int
-{
-    return $this->fav;
-}
+    public function getFav(): int
+    {
+        return $this->fav;
+    }
 
-public function setFav(int $fav): void
-{
-    $this->fav = $fav;
-}
+    public function setFav(int $fav): void
+    {
+        $this->fav = $fav;
+    }
 
-public function getPrice(): int
-{
-    return $this->price;
-}
+    public function getPrice(): int
+    {
+        return $this->price;
+    }
 
-public function setPrice(int $price): void
-{
-    $this->price = $price;
-}
+    public function setPrice(int $price): void
+    {
+        $this->price = $price;
+    }
 
-public function getTva(): int
-{
-    return $this->tva;
-}
+    public function getTva(): int
+    {
+        return $this->tva;
+    }
 
-public function setTva(int $tva): void
-{
-    $this->tva = $tva;
-}
+    public function setTva(int $tva): void
+    {
+        $this->tva = $tva;
+    }
 
-public function getCategory(): ?Category
-{
-    return $this->category;
-}
+    public function getCategory(): ?Category
+    {
+        return $this->category;
+    }
 
-public function setCategory(?Category $category): void
-{
-    $this->category = $category;
-}
+    public function setCategory(?Category $category): void
+    {
+        $this->category = $category;
+    }
 
-public function isShow(): ?bool
-{
-    return $this->show;
-}
+    public function isShow(): ?bool
+    {
+        return $this->show;
+    }
 
-public function setShow(bool $show): static
-{
-    $this->show = $show;
+    public function setShow(bool $show): static
+    {
+        $this->show = $show;
+    }
     /**
      * @return Collection<int, Favorite>
      */
